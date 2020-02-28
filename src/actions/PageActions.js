502aadd4617a290394167900c295cf5c00a06508
Elina Razina <elina.razina@dataart.com>
2020-02-28 17:59:20 +0300

    adding vk script

--- src/actions/PageActions.js
+++ src/actions/PageActions.js
@@ -1,6 +1,6 @@
 export const GET_PHOTOS_REQUEST = "GET_PHOTOS_REQUEST";
 export const REQUEST_SUCCESS = "REQUEST_SUCCESS";
-export const REQUEST_FAILURE = "REQUEST_FAILURE";
+// export const REQUEST_FAILURE = "REQUEST_FAILURE";
 
   export function getPhotos(year){
       return dispatch => {
